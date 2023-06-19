c7652c4cc098507537afd38e87002acc1ede305f
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-06-19 18:30:39 +0100

    Init Project

---
+++ LearnLink/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearnLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
